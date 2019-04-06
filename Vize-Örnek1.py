920e167047a8f7f3dcb6de57c6f6c41ce62f619b
Onur Karabulut <39680969+onurkarabulut@users.noreply.github.com>
2019-04-06 16:52:37 +0300

    matristeki 0 oranını bulan program

--- Vize-Örnek1.py
+++ Vize-Örnek1.py
@@ -1,5 +1,5 @@
-#matristeki '0' oranı %30 un altında ise matrisi hashe atayan program
-a=[[1,2,3,5],[4,5,6,2],[6,5,5,0],[4,0,0,0],[0,5,6,9]]
+#verilen matristeki 0 ların tüm eleman sayısına oranını bulan program
+a=[[1,2,3,5],[4,5,6,0],[0,0,0,0],[0,0,0,0],[0,5,6,9]]
 row=len(a)
 colm=len(a[0])
 max=row*colm
@@ -10,13 +10,7 @@ for i in range(row):
             zCount+=1
 
 percent=(zCount*100)//max
-if(percent<30):
-    my_hash={}
-    for i in range(row):
-        for j in range(colm):
-            my_hash[(i,j)]=a[i][j]
+print("matristeki 0 sayisi :",zCount)
+print("matristeki toplam eleman sayisi: ",max)
+print("matristeki 0 orani %",percent)
 
-    for key in my_hash:
-        print(my_hash[key],end=" ")
-else:
-    print("matristeki 0 orani %",percent)
